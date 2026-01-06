989e115d27f088819b276e605963ece391ad8886
Franck <christian.fga9@gmail.com>
2026-01-06 16:21:07 +0100

    first git

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,98 +1,56 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import Font from '@/constants/Font'
+import FontSize from '@/constants/FontSize'
+import Spacing from '@/constants/Spacing'
+import { Colors } from '@/constants/theme'
+import { tags } from "@/data"
+import { Octicons } from '@expo/vector-icons'
+import React, { useState } from 'react'
+import { ScrollView, Text, TouchableOpacity, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
-import { HelloWave } from '@/components/hello-wave';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { Link } from 'expo-router';
 
-export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <Link href="/modal">
-          <Link.Trigger>
-            <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-          </Link.Trigger>
-          <Link.Preview />
-          <Link.Menu>
-            <Link.MenuAction title="Action" icon="cube" onPress={() => alert('Action pressed')} />
-            <Link.MenuAction
-              title="Share"
-              icon="square.and.arrow.up"
-              onPress={() => alert('Share pressed')}
-            />
-            <Link.Menu title="More" icon="ellipsis">
-              <Link.MenuAction
-                title="Delete"
-                icon="trash"
-                destructive
-                onPress={() => alert('Delete pressed')}
-              />
-            </Link.Menu>
-          </Link.Menu>
-        </Link>
+const Index = () => {
 
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+    const[activeTag,setActivetag] = useState(null)
+
+return (
+
+<SafeAreaView>
+<View style={{ paddingHorizontal: Spacing * 2}}>
+<View style={{ flexDirection:"row", justifyContent:"space-between" }}>
+
+<TouchableOpacity style={{ height: Spacing * 5, width: Spacing * 5, backgroundColor: Colors.dark.background, alignItems:"center", justifyContent:"center", borderRadius: Spacing * 3,}}>
+<Octicons name="apps" size={24} color={Colors.dark.icon}/>
+</TouchableOpacity>
+
+<TouchableOpacity onPress={()=> setActivetag(null) }style={{ height: Spacing * 5, width: Spacing * 5, backgroundColor: Colors.dark.background, alignItems:"center", justifyContent:"center", borderRadius: Spacing * 3,}}>
+<Octicons name="search" size={24} color={Colors.dark.icon}/>   
+</TouchableOpacity>
+</View>
+
+<View style={{marginVertical: Spacing * 2}}>
+<Text style={{fontSize:FontSize.xxLarge, fontFamily:Font["poppins-bold"],width:"70%"}}>I Protect My Humble Home</Text>
+<Text style={{fontSize:FontSize.small, fontFamily:Font["poppins-regular"],width:"70%"}}>Im tell you that</Text>
+</View>
+<ScrollView horizontal showsHorizontalScrollIndicator={false}>
+    {tags.map((tag) =>(
+        <TouchableOpacity style={{ 
+            backgroundColor:Colors.light.background,
+            paddingVertical:Spacing* 2,
+            marginHorizontal: Spacing * 2,
+            paddingHorizontal: Spacing * 3,
+            borderRadius: Spacing * 5,}}
+            key = {tag.id}
+            >
+                <Text style ={{ fontSize: FontSize.large, fontFamily:Font["poppins-semiBold"]}}>{tag.title}</Text>
+            
+        </TouchableOpacity>
+    ))}
+</ScrollView>
+</View> 
+</SafeAreaView>
+
+)
 }
 
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
+export default Index
